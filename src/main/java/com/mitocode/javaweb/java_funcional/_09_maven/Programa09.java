20a1e3229f67a59e1c7ea27b8f664c0354d2778b
Diego Requejo <javaweb@mitocodenetwork.com>
2020-11-07 15:28:54 -0500

    Semana 03 - Sesion 02 - Dependencias con maven

---
+++ src/main/java/com/mitocode/javaweb/java_funcional/_09_maven/Programa09.java
@@ -0,0 +1,10 @@
+package com.mitocode.javaweb.java_funcional._09_maven;
+
+import com.mitocode.javaweb.java_intro.ejercicio12_bonus.ProgramaBonus;
+
+public class Programa09 {
+
+	public static void main(String[] args) {
+		ProgramaBonus.agendaTelefonica();
+	}
+}
